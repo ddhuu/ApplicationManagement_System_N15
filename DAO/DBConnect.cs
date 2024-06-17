9c12092a3bc8e6ee65d7b9afc1b4acf546cbd2cf
ddhuu <ddhuu.dev@gmail.com>
2024-06-17 17:29:46 +0700

    Init Project

---
+++ DAO/DBConnect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class DBConnect : IDisposable
+    {
+        private static Lazy<SqlConnection> lazyConnection = new Lazy<SqlConnection>(() =>
+            new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString));
+
+        public static SqlConnection GetOpenConnection()
+        {
+            var connection = lazyConnection.Value;
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public void Dispose()
+        {
+            if (lazyConnection.IsValueCreated)
+            {
+                lazyConnection.Value.Dispose();
+            }
+        }
+    }
+}
---
+++ PTTK_N15/Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PTTK_N15
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
