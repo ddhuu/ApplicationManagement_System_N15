02d2ef83914966f171a0bbd933f348eecefbc533
QuocHuy3652 <quochuy3652@gmail.com>
2024-07-03 23:14:33 +0700

    fix UI applying job

--- PTTK_N15/Candidate/Applying.Designer.cs
+++ PTTK_N15/Candidate/Applying.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(59, 118);
+            this.label1.Location = new System.Drawing.Point(59, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 31);
             this.label1.TabIndex = 0;
@@ -70,7 +70,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(59, 228);
+            this.label2.Location = new System.Drawing.Point(59, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(113, 31);
             this.label2.TabIndex = 1;
@@ -80,7 +80,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(59, 328);
+            this.label3.Location = new System.Drawing.Point(59, 262);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(97, 31);
             this.label3.TabIndex = 2;
@@ -90,7 +90,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(753, 228);
+            this.label4.Location = new System.Drawing.Point(847, 174);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(135, 31);
             this.label4.TabIndex = 3;
@@ -100,7 +100,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label5.Location = new System.Drawing.Point(753, 118);
+            this.label5.Location = new System.Drawing.Point(847, 75);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 31);
             this.label5.TabIndex = 4;
@@ -110,7 +110,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label6.Location = new System.Drawing.Point(59, 426);
+            this.label6.Location = new System.Drawing.Point(59, 356);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 31);
             this.label6.TabIndex = 5;
@@ -120,7 +120,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label7.Location = new System.Drawing.Point(753, 426);
+            this.label7.Location = new System.Drawing.Point(847, 356);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(81, 31);
             this.label7.TabIndex = 6;
@@ -130,7 +130,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label8.Location = new System.Drawing.Point(59, 722);
+            this.label8.Location = new System.Drawing.Point(59, 602);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(68, 31);
             this.label8.TabIndex = 7;
@@ -147,13 +147,13 @@
             this.nameTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.nameTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.nameTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.nameTB.Location = new System.Drawing.Point(210, 113);
+            this.nameTB.Location = new System.Drawing.Point(210, 70);
             this.nameTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.nameTB.Name = "nameTB";
             this.nameTB.PasswordChar = '\0';
             this.nameTB.PlaceholderText = "";
             this.nameTB.SelectedText = "";
-            this.nameTB.Size = new System.Drawing.Size(420, 60);
+            this.nameTB.Size = new System.Drawing.Size(489, 60);
             this.nameTB.TabIndex = 8;
             // 
             // DOBDTP
@@ -162,11 +162,11 @@
             this.DOBDTP.FillColor = System.Drawing.Color.White;
             this.DOBDTP.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.DOBDTP.Format = System.Windows.Forms.DateTimePickerFormat.Long;
-            this.DOBDTP.Location = new System.Drawing.Point(918, 213);
+            this.DOBDTP.Location = new System.Drawing.Point(1012, 160);
             this.DOBDTP.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.DOBDTP.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.DOBDTP.Name = "DOBDTP";
-            this.DOBDTP.Size = new System.Drawing.Size(420, 60);
+            this.DOBDTP.Size = new System.Drawing.Size(478, 60);
             this.DOBDTP.TabIndex = 9;
             this.DOBDTP.Value = new System.DateTime(2024, 7, 3, 1, 27, 14, 943);
             // 
@@ -181,13 +181,13 @@
             this.CCCDTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.CCCDTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.CCCDTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.CCCDTB.Location = new System.Drawing.Point(918, 113);
+            this.CCCDTB.Location = new System.Drawing.Point(1012, 70);
             this.CCCDTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CCCDTB.Name = "CCCDTB";
             this.CCCDTB.PasswordChar = '\0';
             this.CCCDTB.PlaceholderText = "";
             this.CCCDTB.SelectedText = "";
-            this.CCCDTB.Size = new System.Drawing.Size(420, 60);
+            this.CCCDTB.Size = new System.Drawing.Size(478, 60);
             this.CCCDTB.TabIndex = 10;
             // 
             // phoneNumTB
@@ -201,13 +201,13 @@
             this.phoneNumTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.phoneNumTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.phoneNumTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.phoneNumTB.Location = new System.Drawing.Point(210, 413);
+            this.phoneNumTB.Location = new System.Drawing.Point(210, 340);
             this.phoneNumTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.phoneNumTB.Name = "phoneNumTB";
             this.phoneNumTB.PasswordChar = '\0';
             this.phoneNumTB.PlaceholderText = "";
             this.phoneNumTB.SelectedText = "";
-            this.phoneNumTB.Size = new System.Drawing.Size(420, 60);
+            this.phoneNumTB.Size = new System.Drawing.Size(489, 60);
             this.phoneNumTB.TabIndex = 11;
             // 
             // emailTB
@@ -221,13 +221,13 @@
             this.emailTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.emailTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.emailTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.emailTB.Location = new System.Drawing.Point(918, 413);
+            this.emailTB.Location = new System.Drawing.Point(1012, 340);
             this.emailTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.emailTB.Name = "emailTB";
             this.emailTB.PasswordChar = '\0';
             this.emailTB.PlaceholderText = "";
             this.emailTB.SelectedText = "";
-            this.emailTB.Size = new System.Drawing.Size(420, 60);
+            this.emailTB.Size = new System.Drawing.Size(478, 60);
             this.emailTB.TabIndex = 12;
             // 
             // jobTB
@@ -241,13 +241,13 @@
             this.jobTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.jobTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.jobTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.jobTB.Location = new System.Drawing.Point(210, 706);
+            this.jobTB.Location = new System.Drawing.Point(210, 586);
             this.jobTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.jobTB.Name = "jobTB";
             this.jobTB.PasswordChar = '\0';
             this.jobTB.PlaceholderText = "";
             this.jobTB.SelectedText = "";
-            this.jobTB.Size = new System.Drawing.Size(420, 60);
+            this.jobTB.Size = new System.Drawing.Size(489, 60);
             this.jobTB.TabIndex = 14;
             // 
             // addrTB
@@ -261,13 +261,13 @@
             this.addrTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.addrTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.addrTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.addrTB.Location = new System.Drawing.Point(210, 313);
+            this.addrTB.Location = new System.Drawing.Point(210, 250);
             this.addrTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.addrTB.Name = "addrTB";
             this.addrTB.PasswordChar = '\0';
             this.addrTB.PlaceholderText = "";
             this.addrTB.SelectedText = "";
-            this.addrTB.Size = new System.Drawing.Size(1128, 60);
+            this.addrTB.Size = new System.Drawing.Size(1280, 60);
             this.addrTB.TabIndex = 16;
             // 
             // backBTN
@@ -280,7 +280,7 @@
             this.backBTN.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(195)))), ((int)(((byte)(231)))));
             this.backBTN.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.backBTN.ForeColor = System.Drawing.Color.White;
-            this.backBTN.Location = new System.Drawing.Point(354, 817);
+            this.backBTN.Location = new System.Drawing.Point(387, 709);
             this.backBTN.Name = "backBTN";
             this.backBTN.Size = new System.Drawing.Size(312, 67);
             this.backBTN.TabIndex = 17;
@@ -297,7 +297,7 @@
             this.applyBTN.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(195)))), ((int)(((byte)(231)))));
             this.applyBTN.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.applyBTN.ForeColor = System.Drawing.Color.White;
-            this.applyBTN.Location = new System.Drawing.Point(790, 817);
+            this.applyBTN.Location = new System.Drawing.Point(853, 709);
             this.applyBTN.Name = "applyBTN";
             this.applyBTN.Size = new System.Drawing.Size(312, 67);
             this.applyBTN.TabIndex = 18;
@@ -317,16 +317,16 @@
             this.genderCB.Items.AddRange(new object[] {
             "Nam",
             "Nữ"});
-            this.genderCB.Location = new System.Drawing.Point(210, 213);
+            this.genderCB.Location = new System.Drawing.Point(210, 160);
             this.genderCB.Name = "genderCB";
-            this.genderCB.Size = new System.Drawing.Size(420, 36);
+            this.genderCB.Size = new System.Drawing.Size(489, 36);
             this.genderCB.TabIndex = 19;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label9.Location = new System.Drawing.Point(728, 722);
+            this.label9.Location = new System.Drawing.Point(848, 602);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(182, 31);
             this.label9.TabIndex = 20;
@@ -343,20 +343,20 @@
             this.bussinessTB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.bussinessTB.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.bussinessTB.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.bussinessTB.Location = new System.Drawing.Point(918, 706);
+            this.bussinessTB.Location = new System.Drawing.Point(1038, 586);
             this.bussinessTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.bussinessTB.Name = "bussinessTB";
             this.bussinessTB.PasswordChar = '\0';
             this.bussinessTB.PlaceholderText = "";
             this.bussinessTB.SelectedText = "";
-            this.bussinessTB.Size = new System.Drawing.Size(420, 60);
+            this.bussinessTB.Size = new System.Drawing.Size(452, 60);
             this.bussinessTB.TabIndex = 21;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label10.Location = new System.Drawing.Point(57, 621);
+            this.label10.Location = new System.Drawing.Point(57, 523);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(276, 46);
             this.label10.TabIndex = 22;
@@ -366,7 +366,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label11.Location = new System.Drawing.Point(57, 26);
+            this.label11.Location = new System.Drawing.Point(57, 9);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(176, 46);
             this.label11.TabIndex = 23;
@@ -382,7 +382,7 @@
             this.chooseFileBTN.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(195)))), ((int)(((byte)(231)))));
             this.chooseFileBTN.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.chooseFileBTN.ForeColor = System.Drawing.Color.White;
-            this.chooseFileBTN.Location = new System.Drawing.Point(251, 512);
+            this.chooseFileBTN.Location = new System.Drawing.Point(249, 433);
             this.chooseFileBTN.Name = "chooseFileBTN";
             this.chooseFileBTN.Size = new System.Drawing.Size(185, 58);
             this.chooseFileBTN.TabIndex = 24;
@@ -393,7 +393,7 @@
             // 
             this.fileLabel.AutoSize = true;
             this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.fileLabel.Location = new System.Drawing.Point(59, 526);
+            this.fileLabel.Location = new System.Drawing.Point(57, 447);
             this.fileLabel.Name = "fileLabel";
             this.fileLabel.Size = new System.Drawing.Size(152, 31);
             this.fileLabel.TabIndex = 25;
@@ -404,7 +404,7 @@
             this.fileChosen.AutoSize = true;
             this.fileChosen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.fileChosen.LinkColor = System.Drawing.Color.Blue;
-            this.fileChosen.Location = new System.Drawing.Point(245, 526);
+            this.fileChosen.Location = new System.Drawing.Point(243, 447);
             this.fileChosen.Name = "fileChosen";
             this.fileChosen.Size = new System.Drawing.Size(556, 31);
             this.fileChosen.TabIndex = 26;
@@ -423,7 +423,7 @@
             this.fileCancelBTN.FillColor = System.Drawing.Color.LightGray;
             this.fileCancelBTN.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.fileCancelBTN.ForeColor = System.Drawing.Color.Black;
-            this.fileCancelBTN.Location = new System.Drawing.Point(1236, 526);
+            this.fileCancelBTN.Location = new System.Drawing.Point(1377, 434);
             this.fileCancelBTN.Name = "fileCancelBTN";
             this.fileCancelBTN.Size = new System.Drawing.Size(77, 44);
             this.fileCancelBTN.TabIndex = 27;
@@ -434,7 +434,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1441, 934);
+            this.ClientSize = new System.Drawing.Size(1587, 822);
             this.Controls.Add(this.fileCancelBTN);
             this.Controls.Add(this.fileChosen);
             this.Controls.Add(this.fileLabel);
